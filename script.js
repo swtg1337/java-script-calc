f6b2a4cc636a52020212e62730a29bf877509d6f
Swtg1337 <31216735+swtg1337@users.noreply.github.com>
2021-11-11 22:11:47 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
 function clearall() {
-
     document.getElementById('input').value = "";
     document.getElementById('result').value = "";
 }
 
 function calc() {
-
-    if (document.getElementById('input').value == "")
-        {
-            alert('Введите выражение');
-        } else 
-            { 
-            document.getElementById('result').value = (eval(input.value));
-            }
-       
-    
+    let x = document.getElementById('input').value;
+    if (x == ""){
+        alert('Введите выражение в поле для ввода');
+    } else { 
+        if (x.match (/^\d+[+\-*/]\d+$/ ) != x){
+            alert('Введите корректное выражение')
+        } else {
+            let firstnumber = Number(x.match(/^\d+/));
+            let operator = x.match(/[+\-*/]/);
+            let secondnumber = Number(x.match(/\d+$/));
+            
+            document.getElementById('result').value = (operator == "+") ? firstnumber + secondnumber:
+                (operator == "-") ? firstnumber - secondnumber:
+                (operator == "/") ? firstnumber / secondnumber:
+                firstnumber * secondnumber;
+        }
+    }  
 }
+
+
+
