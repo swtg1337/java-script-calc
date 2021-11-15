f8bcda7204314019148b54131d680db3cdbe8133
Swtg1337 <31216735+swtg1337@users.noreply.github.com>
2021-11-15 13:29:58 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,44 @@
 function clearall() {
     document.getElementById('input').value = "";
     document.getElementById('result').value = "";
+    document.getElementById('error').style.display = "none";
 }
 
 function calc() {
     let x = document.getElementById('input').value;
-    if (x == ""){
-        alert('Введите выражение в поле для ввода');
+    if (!x){
+        document.getElementById('result').value = "";
+        document.getElementById('error').innerHTML = '<b>Ошибка!</br>Поле для ввода пустое</b>';
+        document.getElementById('error').style.display = "block";
     } else { 
-        if (x.match (/^\d+[+\-*/]\d+$/ ) != x){
-            alert('Введите корректное выражение')
+        if (x.match (/^ *\d+ *[+\-*/] *\d+ *$/) != x){
+            document.getElementById('result').value = "";
+            document.getElementById('error').innerHTML = '<b>Ошибка!</br>Введите корректное выражение</b>';
+            document.getElementById('error').style.display = "block";
         } else {
-            let firstnumber = Number(x.match(/^\d+/));
-            let operator = x.match(/[+\-*/]/);
-            let secondnumber = Number(x.match(/\d+$/));
-            
-            document.getElementById('result').value = (operator == "+") ? firstnumber + secondnumber:
-                (operator == "-") ? firstnumber - secondnumber:
-                (operator == "/") ? firstnumber / secondnumber:
-                firstnumber * secondnumber;
+            let firstnumber = Number(x.match(/^ *\d+ */));
+            let operator = x.match(/[-+/*]/);
+            let secondnumber = Number(x.match(/ *\d+ *$/));
+                
+            SwitchResult(operator[0], firstnumber, secondnumber);
         }
     }  
+}
+
+function SwitchResult(a,b,c){
+    switch (a) {
+        case '+':
+            document.getElementById('result').value = b + c;
+            break;
+        case '-':
+            document.getElementById('result').value = b - c;
+            break;
+        case '*':
+            document.getElementById('result').value = b * c;
+            break;
+        case '/': 
+            document.getElementById('result').value = b / c;   
+    }
 }
 
 
