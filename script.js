d696a8961c0a0b1ed962e5be04925acd4f076921
Swtg1337 <31216735+swtg1337@users.noreply.github.com>
2021-11-22 10:09:31 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,51 +1,56 @@
-function clearall() {
+function clearAll() {
     document.getElementById('expression').value = "";
     document.getElementById('result').value = "";
     document.getElementById('error').style.display = "none";
 }
 
-function calc() {
-    let x = document.getElementById('expression').value;
-    if (!x){
-        document.getElementById('result').value = "";
-        document.getElementById('error').innerHTML = '<b>Ошибка!</br>Поле для ввода пустое</b>';
-        document.getElementById('error').style.display = "block";
-    } else { 
-        if (x.match (/^ *\d+ *[+\-*/] *\d+ *$/) != x){
-            document.getElementById('result').value = "";
-            document.getElementById('error').innerHTML = '<b>Ошибка!</br>Введите корректное выражение</b>';
-            document.getElementById('error').style.display = "block";
-        } else {
-            let firstnumber = Number(x.match(/^ *\d+ */));
-            let operator = x.match(/[-+/*]/);
-            let secondnumber = Number(x.match(/ *\d+ *$/));
-                
-            SwitchResult(operator[0], firstnumber, secondnumber);
-        }
-    }  
-}
-
-function displayError (message) {
-    document.getElementById('result').value = "";
+function displayError(message) {
+	document.getElementById('expression').value = "";
     document.getElementById('error').innerHTML = `<b>Ошибка!</br>${message}</b>`;
     document.getElementById('error').style.display = "block";
 }
 
-function SwitchResult(a,b,c){
-    switch (a) {
+function calc() {
+    const x = document.getElementById('expression').value;
+    let error = null;
+    
+    if (!x) {
+        displayError('Поле ввода пустое');
+        
+        return;
+    }
+    
+    const match = x.match (/^\s*(\d+)\s*([+\-*/])\s*(\d+)\s*$/);
+
+		if (!match){
+        displayError('Введите корректное выражение');
+        
+        return;
+    }
+        
+    const op1 = Number(match[1]);
+    const operator = match[2];
+    const op2 = Number(match[3]);
+
+		clearAll();
+    document.getElementById('result').value = processExpression(operator, op1, op2);
+}
+
+function processExpression(operator, op1, op2){
+    switch (operator) {
         case '+':
-            document.getElementById('result').value = b + c;
-            break;
+            return op1 + op2;
         case '-':
-            document.getElementById('result').value = b - c;
-            break;
+						return op1 - op2;
         case '*':
-            document.getElementById('result').value = b * c;
-            break;
+            return op1 * op2;
         case '/': 
-            if (c != 0) {
-            document.getElementById('result').value = b / c;  
-            } else {displayError('На ноль делить нельзя')
-            } 
+            if (op2 == 0) {
+                displayError('На 0 делить нельзя')
+            } else {
+                return op1 / op2;
+            }
+        default:
+        	throw Error(`undefined operator ${operator}`)
     }
 }
